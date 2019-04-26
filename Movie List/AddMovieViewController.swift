fa878a33c7b8e8b49651683f305e2cdcc23d50e9
Hayden Hastings <haydenhastings32@gmail.com>
2019-04-26 10:04:27 -0600

    Added IBOutlet and IBAction.

--- Movie List/AddMovieViewController.swift
+++ Movie List/AddMovieViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class AddMovieViewController: UIViewController {
 
+    var movieController: MovieController?
+    
+    @IBOutlet weak var addMovieTextField: UITextField!
+    @IBAction func addMovieButtonPressed(_ sender: Any) {
+        guard let movie = addMovieTextField.text else { return }
+        
+        movieController?.createMovie(with: movie)
+        
+        addMovieTextField.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
     
     
 }
